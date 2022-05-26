38f712f2726fd029b15530efa049a5fd2f822056
Witold Frącek <witek.fracek@gmail.com>
2022-05-26 18:35:31 +0200

    player init

---
+++ OnlyPWRfullSurvive/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerMovement : MonoBehaviour
+{
+    // Params
+    public int movmentSpeed = 10;
+
+    [SerializeField] InputActionMap actionMap;
+    
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        
+    }
+
+    // Movement
+
+}
